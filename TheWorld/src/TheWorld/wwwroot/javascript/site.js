5c6923f0669d6acc326327db42dc67a3fdbb96a5
mink <minkatha01@gmail.com>
2016-08-26 15:09:49 -0500

    TheWorld project

---
+++ TheWorld/src/TheWorld/wwwroot/javascript/site.js
@@ -0,0 +1,17 @@
+﻿/* Site.js */
+
+(function () {
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sideBarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        $sidebarAndWrapper.toggleClass("slide-wrapper");
+
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        }
+        else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+})();
