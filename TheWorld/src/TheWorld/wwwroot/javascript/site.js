b310d7afe3995f3a41e963f50edce9dd0e8321bf
mink <minkatha01@gmail.com>
2016-08-29 14:57:11 -0500

    Styling from bootstrap

--- TheWorld/src/TheWorld/wwwroot/javascript/site.js
+++ TheWorld/src/TheWorld/wwwroot/javascript/site.js
@@ -2,16 +2,21 @@
 
 (function () {
     var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    var $icon = $("#sideBarToggle i.fa");
+    var $footer = $("#footer");
 
     $("#sideBarToggle").on("click", function () {
         $sidebarAndWrapper.toggleClass("hide-sidebar");
         $sidebarAndWrapper.toggleClass("slide-wrapper");
+        $footer.toggleClass("slide-footer");
 
         if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
-            $(this).text("Show Sidebar");
+            $icon.removeClass("fa-angle-left");
+            $icon.addClass("fa-angle-right");
         }
         else {
-            $(this).text("Hide Sidebar");
+            $icon.removeClass("fa-angle-right");
+            $icon.addClass("fa-angle-left");
         }
     });
 })();
