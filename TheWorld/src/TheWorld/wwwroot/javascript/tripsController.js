05e0df2a1be1cd665812e7dfe9cad29f6bd768da
mink <minkatha01@gmail.com>
2016-08-31 14:59:43 -0500

    Angular get and post requests

---
+++ TheWorld/src/TheWorld/wwwroot/javascript/tripsController.js
@@ -0,0 +1,55 @@
+﻿// tripsController.js
+(function () {
+
+    "use strict";
+
+    // Getting the existing module
+    angular.module("app-trips")
+        .controller("tripsController", tripsController);
+
+    function tripsController($http) {
+        var viewModel = this;
+
+        viewModel.trips = [];
+
+        viewModel.newTrip = {};
+
+        viewModel.errorMessage = "";
+        viewModel.isBusy = true;
+
+        /* Get all trips */
+        $http.get("/api/trips")
+            .then(function (response) {
+                // Success
+                angular.copy(response.data, viewModel.trips);
+            }, function (error) {
+                // Failure
+                viewModel.errorMessage = "Failed to load data";
+            })
+        .finally(function () {
+            viewModel.isBusy = false;
+        });
+
+        /* Add a trip */
+        viewModel.addTrip = function () {
+
+            viewModel.isBusy = true;
+            viewModel.errorMessage = "";
+
+            $http.post("/api/trips", viewModel.newTrip)
+                .then(function (response) {
+                    // Success
+                    viewModel.trips.push(response.data);
+                    viewModel.newTrip = {};
+                }, function (error) {
+                    // Failure
+                    viewModel.errorMessage = "Failed to save new trip";
+                })
+                .finally(function () {
+                    viewModel.isBusy = false;
+                });
+        };
+
+
+    }
+})();
